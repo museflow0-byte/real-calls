3cc4b5e5c63b7de269c58fc6320e7659214cf798
museflow0-byte <museflow0@gmail.com>
2025-09-30 16:38:32 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import axios from "axios";
+import axios from 'axios';
 import path from "path";
 import { fileURLToPath } from "url";
 
